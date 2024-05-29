91562da97b796e020811c49b310dd3a8372ccd8a
Arsid Kuljici <arsid@hotmail.ch>
2024-05-29 15:27:55 +0200

    Consumer.py wird so angepasst, dass cunsumer name in docker-compose als vairbale bzw. envariement definiert wird. In source (consumer.py) wird consumer name als Placeholder definiert.

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 # Environment variables
 rabbitmq_url = os.getenv('RABBITMQ_URL', 'amqp://stockmarket:supersecret123@localhost:5672/')
 mongodb_url = os.getenv('MONGODB_URL', 'mongodb://mongo:27017/')
-queue_names = ['AAPL', 'MSFT', 'TSLA']
+queue_name = os.getenv('QUEUE_NAME')  # Only consume from the specified queue
 
 # Wait for RabbitMQ to become available
 while True:
@@ -21,9 +21,8 @@ while True:
 
 channel = connection.channel()
 
-# Ensure the queues exist
-for queue_name in queue_names:
-    channel.queue_declare(queue=queue_name, durable=True)
+# Ensure the queue exists
+channel.queue_declare(queue=queue_name, durable=True)
 
 mongo_client = pymongo.MongoClient(mongodb_url)
 db = mongo_client.stockmarket
@@ -32,31 +31,24 @@ collection = db.stocks
 buffers = defaultdict(list)
 buffer_size = 1000
 
-
 def process_buffer(queue_name):
     if len(buffers[queue_name]) >= buffer_size:
         avg_price = sum(item['price'] for item in buffers[queue_name]) / buffer_size
         aggregated_data = {
             'company': queue_name,
-            'avg_price': avg_price,
-            'count': buffer_size,
-            'timestamp': time.time()
-        }
+            'avg_price': avg_price
+            }
         collection.insert_one(aggregated_data)
         buffers[queue_name].clear()
 
-
 def callback(ch, method, properties, body):
     message = json.loads(body)
-    queue_name = method.routing_key
     buffers[queue_name].append(message)
 
     process_buffer(queue_name)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+channel.basic_consume(queue=queue_name, on_message_callback=callback)
 
-for queue_name in queue_names:
-    channel.basic_consume(queue=queue_name, on_message_callback=callback)
-
-print('Waiting for messages...')
+print(f'Waiting for messages from {queue_name}...')
 channel.start_consuming()
